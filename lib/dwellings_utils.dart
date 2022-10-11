699ed69fb8f09fc1ac10623350484cf1f1a13ef6
EhisEA <37916226+EhisEA@users.noreply.github.com>
2022-10-11 17:34:12 +0100

    chore: export gap

--- lib/dwellings_utils.dart
+++ lib/dwellings_utils.dart
@@ -7,4 +7,5 @@ export 'src/utils/color.dart';
 export 'src/text/app_text.dart';
 export 'src/text/textstyle.dart';
 export 'src/text/two_text.dart';
+export 'src/gap.dart';
 export 'src/textfield.dart';
